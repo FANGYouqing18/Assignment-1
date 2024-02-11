bc0024a3efb826ce63ef215ba6a2bde943ad5f96
FANG Youqing <23401079@life.hkbu.edu.hk>
2024-02-11 16:14:12 +0800

    updates for output info

---
+++ clean.py
@@ -0,0 +1,26 @@
+import pandas as pd
+import sys
+
+def clean_data(input1, input2, output):
+    df_contact = pd.read_csv(input1)
+    df_other = pd.read_csv(input2)
+
+    df_merged = pd.merge(df_contact, df_other, left_on='respondent_id', right_on='id')
+
+    df_merged.drop('id', axis=1, inplace=True)
+
+    df_merged.dropna(inplace=True)
+
+    df_merged = df_merged[~df_merged['job'].str.contains('insurance', case=False)]
+
+    df_merged.to_csv(output, index=False)
+
+    print("Output file shape:", df_merged.shape)
+    
+if __name__ == "__main__":
+    if len(sys.argv) != 4:
+        print("Usage: python clean.py <input1> <input2> <output>")
+        sys.exit(1)
+
+    input1, input2, output = sys.argv[1], sys.argv[2], sys.argv[3]
+    clean_data(input1, input2, output)
